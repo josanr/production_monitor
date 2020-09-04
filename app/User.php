84b9b8688fa2fe1993950029d45a5ba75a98581e
ruslan <josanr@gmail.com>
2020-09-04 15:27:33 +0300

    change the right class to use

--- app/User.php
+++ app/User.php
@@ -2,46 +2,50 @@
 
 namespace App;
 
-use Barryvdh\LaravelIdeHelper\Eloquent;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
 /**
  * App\User
  *
- * @property int $id
- * @property string $name
- * @property string|null $email
- * @property string $password
- * @property string $displayname
- * @property int $rnk
- * @property int $department
- * @property int $filial
- * @property int $process_type
- * @property int $region_id
- * @property string|null $last_login
- * @property string|null $remember_token
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
- * @property-read int|null $notifications_count
- * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereDepartment($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereDisplayname($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereFilial($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLogin($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereProcessType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRegionId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRnk($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @property int                                                        $id
+ * @property string                                                     $name
+ * @property string|null                                                $email
+ * @property string                                                     $password
+ * @property string                                                     $displayname
+ * @property int                                                        $rnk
+ * @property int                                                        $department
+ * @property int                                                        $filial
+ * @property int                                                        $process_type
+ * @property int                                                        $region_id
+ * @property string|null                                                $last_login
+ * @property string|null                                                $remember_token
+ * @property Carbon|null                                                $created_at
+ * @property Carbon|null                                                $updated_at
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null                                              $notifications_count
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereDepartment($value)
+ * @method static Builder|User whereDisplayname($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereFilial($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereLastLogin($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereProcessType($value)
+ * @method static Builder|User whereRegionId($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereRnk($value)
+ * @method static Builder|User whereUpdatedAt($value)
  * @mixin Eloquent
  */
 class User extends Authenticatable
