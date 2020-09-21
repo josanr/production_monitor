18362934b163f68d98d409ad8b5357ba44c6b210
ruslan <josanr@gmail.com>
2020-09-21 15:10:18 +0300

    add state to entity order

--- app/Application/Entities/Order.php
+++ app/Application/Entities/Order.php
@@ -10,6 +10,16 @@ class Order
      * @var int
      */
     private $id;
+    /**
+     * @var int
+     */
+    private $status;
+    /**
+     * @var string
+     */
+    private $name;
+    /** @var int */
+    private $filial;
 
     /**
      * Order constructor.
@@ -19,5 +29,28 @@ class Order
     public function __construct(int $id)
     {
         $this->id = $id;
+        $this->status = 10;
+        $this->name = "09/10";
+        $this->filial = 1;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getFilial()
+    {
+        return $this->filial;
     }
 }
